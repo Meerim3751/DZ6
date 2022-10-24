9c383097cd7832afa9f6f3470d1691d0d46036e3
Meerim15 <meka3751@gmail.com>
2022-10-24 16:25:41 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const block = {
+  height: 200,
+  width: 300,
+  title: "My block"
+};
+
+const result = Math.pow(200,2)
+console.log(result)
+
+const result2 = Math.pow(300,2)
+console.log(result2)
